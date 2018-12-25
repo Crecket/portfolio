79135b2ad820999e7496ad23b78de1124f983906
Gregory Goijaerts <crecketgaming@gmail.com>
2018-12-25 23:32:26 +0100

    Change styling

--- src/Pages/Projects/ProjectItem.jsx
+++ src/Pages/Projects/ProjectItem.jsx
@@ -5,12 +5,12 @@ const ProjectItem = ({ title, description, image }) => {
     return (
         <div className="project-item">
             <div className="project-content">
-                <img src={image} alt={`${title} project`} />
-
                 <div className="text-content">
                     <h3>{title}</h3>
                     <p>{description}</p>
                 </div>
+
+                <img src={image} alt={`${title} project`} />
             </div>
         </div>
     );
