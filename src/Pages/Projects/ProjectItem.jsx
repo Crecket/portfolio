5b8e2e747d69d8e4c052d4abc7703bb7ebd7df58
Gregory Goijaerts <crecketgaming@gmail.com>
2018-12-27 23:24:47 +0100

    Initial project page setup finished

--- src/Pages/Projects/ProjectItem.jsx
+++ src/Pages/Projects/ProjectItem.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import "./ProjectItem.scss";
 
-const ProjectItem = ({ title, description, image }) => {
+import DownloadIcon from "../../SVGImages/Download";
+import AccountGroupIcon from "../../SVGImages/AccountGroup";
+import AccountMultipleIcon from "../../SVGImages/AccountMultiple";
+
+const DownloadCount = ({ downloads }) => {
+    if (!downloads) return null;
+
+    const formattedDownloads = downloads.toLocaleString();
+
+    return (
+        <p>
+            <DownloadIcon /> ~ {formattedDownloads} Downloads
+        </p>
+    );
+};
+const ViewCount = ({ views }) => {
+    if (!views) return null;
+
+    const formattedViews = views.toLocaleString();
+
+    return (
+        <p>
+            <AccountGroupIcon /> ~ {formattedViews} views/month
+        </p>
+    );
+};
+
+const ProjectItem = ({ title, description, image, url, downloadCount, viewCount }) => {
+    const imageComponent = <img src={image} alt={`${title} project`} />;
     return (
         <div className="project-item">
             <div className="project-content">
@@ -10,7 +38,18 @@ const ProjectItem = ({ title, description, image }) => {
                     <p>{description}</p>
                 </div>
 
-                <img src={image} alt={`${title} project`} />
+                <div className="bottom-content">
+                    <DownloadCount downloads={downloadCount} />
+                    <ViewCount views={viewCount} />
+                </div>
+
+                {url ? (
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                        {imageComponent}
+                    </a>
+                ) : (
+                    imageComponent
+                )}
             </div>
         </div>
     );
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,29 +1,61 @@
 import React from "react";
-// import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet";
 import "./Projects.scss";
 
 import LandingSection from "../../Components/LandingSection";
-import useScrollPosition from "../../Hooks/useScrollPosition.js";
-
 import ProjectItem from "./ProjectItem";
 
 import bunqDesktopImage from "./images/bunq-desktop.png";
+import masteryPointsImage from "./images/masterypoints.png";
+import bunqJSClientImage from "./images/bunq-js-client.png";
+import bunqCLIImage from "./images/bunq-cli.png";
+
+const projects = [
+    {
+        title: "bunqDesktop",
+        description: "The unofficial, free and open source desktop application for the bunq API",
+        image: bunqDesktopImage,
+        url: "https://bunqdesk.top/",
+        downloadCount: 15000
+    },
+    {
+        title: "Masterypoints",
+        description:
+            "MasteryPoints lets you view summoner profiles, compare summoners and view your rankings on the highscores for league of legends",
+        image: masteryPointsImage,
+        url: "https://www.masterypoints.com/",
+        viewCount: 750000
+    },
+    {
+        title: "bunqJSClient",
+        description: "A javascript SDK for the bunq API - was originally built for the web but has support for NodeJS",
+        image: bunqJSClientImage,
+        url: "https://github.com/bunqCommunity/bunqJSClient",
+        downloadCount: 27000
+    },
+    {
+        title: "bunq-cli",
+        description: "An unofficial and open source CLI tool to quickly use the bunq API",
+        image: bunqCLIImage,
+        url: "https://github.com/bunqCommunity/bunq-cli"
+    }
+];
 
 const Projects = () => {
-    const scrollPosition = useScrollPosition();
-    console.log(scrollPosition);
-
     return (
         <div className="projects">
-            {/*<Helmet>*/}
-            {/*<title>GregDev - Projects</title>*/}
-            {/*</Helmet>*/}
+            <Helmet>
+                <title>GregDev - Projects</title>
+            </Helmet>
+
             <LandingSection className="text-wrapper" height="40vh" displayHomeLink>
                 <h1>Projects</h1>
             </LandingSection>
 
             <div className="project-section">
-                <ProjectItem image={bunqDesktopImage} title="bunqDesktop" description="testDescription" />
+                {projects.map(project => (
+                    <ProjectItem key={project.title} {...project} />
+                ))}
             </div>
         </div>
     );
