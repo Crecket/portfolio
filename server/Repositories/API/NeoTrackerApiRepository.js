c33f5d457e086edf52cf18d38376c0b2a7d46e59
Crecket <crecketgaming@gmail.com>
2019-03-01 09:59:06 +0100

    Remove backend

--- server/Repositories/API/NeoTrackerApiRepository.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-class NeoTrackerApiRepository {
-    constructor() {}
-
-    // https://neotracker.io/address/AWhEJ7pV1HC2KemXrybtz1r3UzxrAmmWMg
-}
-
-export default NeoTrackerApiRepository;
