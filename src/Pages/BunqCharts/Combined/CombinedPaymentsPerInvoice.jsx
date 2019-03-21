ab429040846178033c09fb5a474e751b6fc2ae6c
Gregory Goijaerts <ggoijaerts@zig.nl>
2019-03-21 12:49:25 +0100

    Add new combined test chart

--- src/Pages/BunqCharts/Combined/CombinedPaymentsPerInvoice.jsx
+++ src/Pages/BunqCharts/Combined/CombinedPaymentsPerInvoice.jsx
@@ -6,6 +6,8 @@ import StandardChartOptions from "../StandardChartOptions";
 
 export default ({ bunqData }) => {
     const dataSet = [];
+    const dataSet2 = [];
+    let previousPaymentId = 0;
     bunqData.invoices.forEach(invoice => {
         const invoiceDate = new Date(invoice.date);
 
@@ -22,34 +24,86 @@ export default ({ bunqData }) => {
         if (nextPayment) {
             const previousPayment = bunqData.payments[previousIndex];
             const averagePaymentId = (nextPayment.id + previousPayment.id) / 2;
-            const invoicesPerPayment = Math.round(averagePaymentId / invoice.id);
+            const paymentIdChange = averagePaymentId - previousPaymentId;
+            previousPaymentId = averagePaymentId;
+
+            // go to next round, don't add this invoice
+            if (invoice.changeAdjusted === 0) return;
+
+            const invoicesPerPayment = Math.round(paymentIdChange / invoice.changeAdjusted);
 
             dataSet.push({
                 x: invoiceDate,
                 y: invoicesPerPayment
             });
+            dataSet2.push({
+                x: invoiceDate,
+                y: paymentIdChange
+            });
         }
     });
+
+    const options = { ...StandardChartOptions() };
+    options.scales.yAxes[0] = {
+        ...options.scales.yAxes[0],
+        id: "invoice-per-payment",
+        gridLines: {
+            display: true,
+            color: "rgba(103, 77, 255, 0.6)"
+        }
+    };
+    options.scales.yAxes[1] = {
+        id: "payments-change",
+        type: "linear",
+        position: "right",
+        fontColor: "white",
+        ticks: {
+            fontColor: "white",
+            beginAtZero: true,
+            callback: function(value, index, values) {
+                return value.toLocaleString();
+            }
+        },
+        gridLines: {
+            display: true,
+            color: "rgba(103, 255, 77, 0.6)"
+        }
+    };
+
+    // remove the last month since it isn't complete and the amonut of payments will be lower
+    dataSet.pop();
+    dataSet2.pop();
 
     return (
         <div>
             <Typography variant="body1" className="chart-description">
-                The amount of payments which have been done VS the currently total amount of invoices.
+                The amount of payments which have been done VS the amount of invoices for that month.
             </Typography>
             <Line
                 data={{
                     datasets: [
                         {
-                            label: `Payments per invoice`,
+                            label: `Invoices per payment`,
                             data: dataSet,
-                            fill: true,
+                            fill: false,
                             pointHitRadius: 1,
-                            backgroundColor: "#0d61e8",
-                            pointBackgroundColor: "#0d61e8"
+                            backgroundColor: "rgba(13, 97, 232, 0.6)",
+                            // backgroundColor: "#0d61e8",
+                            pointBackgroundColor: "#0d61e8",
+                            yAxisID: "invoice-per-payment"
+                        },
+                        {
+                            label: `Payments change`,
+                            data: dataSet2,
+                            fill: false,
+                            pointHitRadius: 1,
+                            backgroundColor: "rgba(103, 255, 77, 0.6)",
+                            pointBackgroundColor: "#67ff4d",
+                            yAxisID: "payments-change"
                         }
                     ]
                 }}
-                options={StandardChartOptions()}
+                options={options}
             />
         </div>
     );
