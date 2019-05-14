8b2118168ad97676f0ee23268e18a8c34723a253
Gregory Goijaerts <crecketgaming@gmail.com>
2019-05-14 23:00:07 +0200

    Improve annotation list

--- src/Pages/Bunq/InvoiceIDs/InvoiceIDChangeChart.jsx
+++ src/Pages/Bunq/InvoiceIDs/InvoiceIDChangeChart.jsx
@@ -93,11 +93,7 @@ export default ({ invoices }) => {
             </Typography>
             <div className="chart-content">
                 <DefaultSwitch label="Use 5 month moving average" checked={movingAverage} onChange={setMovingAverage} />
-                <DefaultSwitch
-                    label="Show annotations"
-                    checked={showAnnotations}
-                    onChange={setShowAnnotations}
-                />
+                <DefaultSwitch label="Show annotations" checked={showAnnotations} onChange={setShowAnnotations} />
                 <TextField
                     min="0"
                     type="number"
--- src/Pages/Bunq/StandardAnnotations.js
+++ src/Pages/Bunq/StandardAnnotations.js
@@ -1,23 +1,20 @@
-export const genericEvents = [
-    { label: "Premium", date: new Date("2017-08-02") },
-    { label: "Pack", date: new Date("2017-11-27") },
-    { label: "Apple Pay", date: new Date("2018-03-20") },
-    { label: "ZeroFX", date: new Date("2018-11-06") },
-    { label: "Cards", date: new Date("2018-10-08"), yAdjust: 60 },
-    { label: "MassInterest", date: new Date("2019-02-26") }
-];
+const defaultGenericOptions = { yAdjust: 30, color: "green" };
+export const genericEvents = [{ label: "Offline Cards", date: new Date("2018-10-08"), ...defaultGenericOptions }];
 
-const defaultBunqOptions = { yAdjust: 30, color: "green" };
+const defaultBunqOptions = {};
 export const bunqUpdates = [
-    { label: "#9", date: new Date("2018-11-15"), ...defaultBunqOptions },
-    { label: "#8", date: new Date("2018-07-15"), ...defaultBunqOptions },
-    { label: "#7", date: new Date("2018-03-15"), ...defaultBunqOptions },
-    { label: "#6", date: new Date("2017-11-15"), ...defaultBunqOptions },
-    { label: "#5", date: new Date("2017-08-15"), ...defaultBunqOptions },
-    { label: "#4", date: new Date("2016-11-15"), ...defaultBunqOptions },
-    { label: "#3", date: new Date("2016-05-15"), ...defaultBunqOptions }
+    { label: "#10 MassInterest", date: new Date("2019-02-26"), ...defaultBunqOptions },
+    { label: "#9 ZeroFX", date: new Date("2018-11-06"), ...defaultBunqOptions },
+    { label: "#8 bunq.me", date: new Date("2018-07-15"), ...defaultBunqOptions },
+    { label: "#7 Apple Pay", date: new Date("2018-03-20"), ...defaultBunqOptions },
+    { label: "#6 Pack", date: new Date("2017-11-27"), ...defaultBunqOptions },
+    { label: "#5 Premium", date: new Date("2017-08-02"), ...defaultBunqOptions },
+    { label: "#4 Slice", date: new Date("2016-11-15"), ...defaultBunqOptions },
+    { label: "#3 Business", date: new Date("2016-05-15"), ...defaultBunqOptions }
+    // { label: "#2 Public", date: new Date("2015-11-15"), ...defaultBunqOptions },
+    // { label: "#1 Beta", date: new Date("2015-09-15"), ...defaultBunqOptions },
 ];
-export const combinedEventList = [...genericEvents, ...bunqUpdates];
+export const combinedEventList = [...bunqUpdates, ...genericEvents];
 
 export const eventsToAnnotations = eventList => {
     return eventList.map(event => {
@@ -30,14 +27,14 @@ export const eventsToAnnotations = eventList => {
             borderWidth: 2,
             label: {
                 content: event.label,
-                backgroundColor: event.color || "rgba(31,96,255)",
+                backgroundColor: event.color || "#1F60FF",
                 fontSize: 12,
                 fontStyle: "bold",
                 fontColor: "#fff",
 
-                xPadding: 6,
-                yPadding: 6,
-                cornerRadius: 6,
+                xPadding: 5,
+                yPadding: 5,
+                cornerRadius: 4,
 
                 // top, bottom, left, right, center. Default below.
                 position: "top",
