f836975b45ba80c0083963764d815b36728155fc
Gregory Goijaerts <crecketgaming@gmail.com>
2019-05-25 12:52:38 +0200

    Change release console log style

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/bash
 
-echo -e "\n  -> Stop PM2 server\n"
+echo -e "\n === Stop PM2 server\n"
 pm2 stop portfolio
 
-echo -e "\n  -> Checking and optimizing image sizes\n"
+echo -e "\n === Checking and optimizing image sizes\n"
 yarn run optimize-images
 
-echo -e "\n  -> Update bunq data files and check API\n"
+echo -e "\n === Update bunq data files and check API\n"
 yarn run generate-bunq-data
 
-echo -e "\n  -> Render bunq charts as images\n"
+echo -e "\n === Render bunq charts as images\n"
 yarn run generate-chart-images
 
-echo -e "\n  -> Build server\n"
+echo -e "\n === Build server\n"
 yarn run build:server
 
-echo -e "\n  -> Build client\n"
+echo -e "\n === Build client\n"
 yarn run build:client
 
-echo -e "\n  -> Start PM2 server\n"
+echo -e "\n === Start PM2 server\n"
 pm2 start portfolio
