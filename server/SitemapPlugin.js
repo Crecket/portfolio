aef74926b680294679effd5abdca4cc4b69436c5
Gregory Goijaerts <crecketgaming@gmail.com>
2019-05-26 16:22:26 +0200

    Improve sitemap coverage

--- server/SitemapPlugin.js
+++ server/SitemapPlugin.js
@@ -3,25 +3,36 @@ const sitemap = require("sitemap");
 
 const routes = require("../src/Config/routes");
 const customSettings = {
-    Home: { priority: 0.7 },
-    Bunq: { changefreq: "daily", priority: 0.5 }
+    Home: { priority: 1 },
+    Contact: { priority: 0.7 },
+    Projects: { priority: 0.7 },
+    Bunq: { changefreq: "weekly", priority: 0.5 }
 };
 
 const sitemapPlugin = (fastify, options, next) => {
     // map url config to a useable sitemap config
-    const urls = Object.keys(routes)
+    const urls = [];
+    Object.keys(routes)
         .filter(routeName => {
             const routeDetails = routes[routeName];
 
             return routeDetails.ignoreSitemap !== true;
         })
-        .map(routeName => {
+        .forEach(routeName => {
             const routeDetails = routes[routeName];
             const extraSettings = customSettings[routeName] || {};
 
             const sitemapUrl = routeDetails.cleanUrl || routeDetails.path;
 
-            return { url: sitemapUrl, ...extraSettings };
+            // add the main path to the list
+            urls.push({ url: sitemapUrl, ...extraSettings });
+
+            // check for subpaths
+            if (Array.isArray(routeDetails.subPaths)) {
+                routeDetails.subPaths.forEach(subPath => {
+                    urls.push({ url: `${sitemapUrl}${subPath}`, priority: 0.2 });
+                });
+            }
         });
 
     // create sitemap instance
--- src/Config/routes.js
+++ src/Config/routes.js
@@ -2,7 +2,11 @@ const routes = {
     Home: { path: "/" },
     Projects: { path: "/projects" },
     Contact: { path: "/contact" },
-    Bunq: { path: "/bunq/:tab?", cleanUrl: "/bunq" },
+    Bunq: {
+        path: "/bunq/:tab?",
+        cleanUrl: "/bunq",
+        subPaths: ["/invoices", "/payments", "/combined", "/together", "/predictions", "/images"]
+    },
     NotFound: { path: "/notfound", ignoreSitemap: true }
 };
 
