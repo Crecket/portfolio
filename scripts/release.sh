8baf3aabec9e8da1b98c54773925a76087752069
Gregory Goijaerts <crecketgaming@gmail.com>
2026-03-15 20:28:36 +0100

    add static data for datasets

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/bash
 
-echo -e "\n === Stop PM2 server\n"
-pm2 stop portfolio
-
-echo -e "\n === Checking and optimizing image sizes\n"
-yarn run optimize-images
-
-echo -e "\n === Update bunq data files and check API\n"
-yarn run generate-bunq-data
-
-echo -e "\n === Render bunq charts as images\n"
-yarn run generate-chart-images
-
-echo -e "\n === Build server\n"
-yarn run build:server
-
-echo -e "\n === Build client\n"
-yarn run build:client
-
-echo -e "\n === Start PM2 server\n"
-pm2 start portfolio
+#echo -e "\n === Stop PM2 server\n"
+#pm2 stop portfolio
+#
+#echo -e "\n === Checking and optimizing image sizes\n"
+#pnpm run optimize-images
+#
+#echo -e "\n === Update bunq data files and check API\n"
+#pnpm run generate-bunq-data
+#
+#echo -e "\n === Render bunq charts as images\n"
+#pnpm run generate-chart-images
+#
+#echo -e "\n === Build app\n"
+#pnpm run build
+#
+#echo -e "\n === Start PM2 server\n"
+#pm2 start portfolio
---
+++ src/DataSets/bunqDataSets.js
@@ -0,0 +1,21 @@
+import * as path from "path";
+import glob from "glob";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ignoredFiles = [path.normalize(`${__dirname}${path.sep}together.json`)];
+
+export default async (directory) => {
+    const files = glob.sync(`${directory}/greg.json`);
+
+    const dataSets = [];
+    for (const file of files) {
+        if (!ignoredFiles.includes(file)) {
+            const { default: data } = await import(file, { assert: { type: "json" } });
+            dataSets.push(data);
+        }
+    }
+    return dataSets;
+};
